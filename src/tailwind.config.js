33d5f54ba5b237dd108d466e95504bd51a479e83
maxim-sivakon <m.sivakon@yandex.com>
2024-08-02 00:44:43 +0700

    save

---
+++ src/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './bin/**/*.css',
+    './html/**/*.html',
+    './assets/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Gotham-Pro']
+      },
+      screens: {
+        'm': '280px',
+        's': '360px',
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
+}
